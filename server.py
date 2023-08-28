922a935520ec3f398fb20d9a5550291799677007
Lussati Santos <lussatisantos10@gmail.com>
2023-08-28 15:48:49 +0100

    act

--- server.py
+++ server.py
@@ -9,14 +9,16 @@ def upserver():
     global target
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsocket(socket.SQL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(['192.168.18.10', 7777])
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(('192.168.18.10', 7777))
     server.listen(1)
 
     print('Servidor processando, esperando uma conexao...')
 
-    target, ip = socket.accept()
+    target, ip = server.accept()
     print(f'Conexao recebida {ip[0]}')
+
+
 
 upserver()
 server.close()
